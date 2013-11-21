2cfc595f6de1a058b4dc897dbe9d35753d78d454
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-21 16:28:10 +0100

    create a base object

---
+++ test/object_test.js
@@ -0,0 +1,99 @@
+var expect = require('chai').expect;
+
+var BaseObject = require('../lib/object');
+
+describe('BaseObject', function(){
+    it('should exist', function(){
+	expect(BaseObject).to.exist;
+    });
+
+    it('should be constructor', function(){
+	expect(BaseObject).to.be.a('function');
+    });
+
+    it('should be an instance of \'Listener\'', function(){
+	expect(new BaseObject()).to.be.an.instanceof(require('asteroids-listener'));
+    });
+
+    describe('object', function(){
+	var base;
+
+	beforeEach(function(){
+	    base = new BaseObject();
+	});
+
+	['x', 'y', 'position'].forEach(function(methodName){
+	    it('should respond to ' + methodName, function(){
+		expect(base).to.respondTo(methodName);
+	    });
+	});
+    })
+
+    describe('interactions', function(){
+	var base;
+
+	beforeEach(function(){
+	    base = new BaseObject();
+	});
+
+	it('should have a default \'x\'', function(){
+	    expect(base.x()).to.equal(0);
+	});
+
+	it('should be able to set \'x\'', function(){
+	    var value = 1;
+
+	    base.x(value);
+
+	    expect(base.x()).to.equal(value);
+	});
+
+	it('should have a default \'y\'', function(){
+	    expect(base.y()).to.equal(0);
+	});
+
+	it('should be able to set \'y\'', function(){
+	    var value = 1;
+
+	    base.y(value);
+
+	    expect(base.y()).to.equal(value);
+	});
+
+	it('should have a default \'position\'', function(){
+	    var position = base.position();
+
+	    expect(position).to.deep.equal({ 'x': 0, 'y': 0 });
+	});
+
+	it('should be able to set \'position\'', function(){
+	    var value = { 'x': 1, 'y': 1 };
+
+	    base.position(value);
+
+	    expect(base.position()).to.deep.equal(value);
+	});
+
+	it('should be able partially update \'position\'', function(){
+	    base.position({ x: 1 });
+	    expect(base.position()).to.deep.equal({ x: 1, y: 0 });
+
+	    base.position({ y: 1 });
+	    expect(base.position()).to.deep.equal({ x: 1, y: 1 });
+	});
+
+	[
+	    function(base){ base.x(1); },
+	    function(base){ base.y(1); },
+	    function(base){ base.position({ x: 1 }); },
+	    function(base){ base.position({ y: 1 }); },
+	    function(base){ base.position({ x: 1, y: 1 }); }
+	].forEach(function(mutator){
+	    it('should notify upon changes', function(done){
+		base.addListener('position', function(){ done(); });
+
+		mutator(base);
+	    });
+	});
+    });
+});
